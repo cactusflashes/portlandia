7459f827bc069da70893b18093b96e6ae329db23
Joseph <121457318+cactusflashes@users.noreply.github.com>
2023-10-18 21:51:54 -0700

    October 18th Updates and Design Edits

--- src/routes/ai/+page.svelte
+++ src/routes/ai/+page.svelte
@@ -6,10 +6,13 @@
 
 
 <script>
+    import { fly } from 'svelte/transition'; 
+    import { cubicInOut } from "svelte/easing";
+
     let text = "Universities are racing to address AI because it is a rapidly advancing technology that is reshaping industries, from healthcare to finance. To stay relevant, institutions must provide cutting-edge education in AI to meet the demands of students and industry. Furthermore, AI has profound implications for research, and universities aim to be at the forefront of AI development to ensure they can contribute to and shape its ethical, societal, and economic dimensions.";
     let outputText = "";
     let currentIndex = 0;
-    let typingSpeed = 20; // Adjust the typing speed (milliseconds per character)
+    let typingSpeed = 10; // Adjust the typing speed (milliseconds per character)
     let isTyping = false;
   
     function type() {
@@ -29,10 +32,20 @@
       if (!isTyping) {
         outputText = "";
         currentIndex = 0;
+        browserVisible = true; 
         isTyping = true;
+        setTimeout(function(){
+          nextSec = true; 
+        }, 6000); 
         type();
       }
     }
+
+
+    let browserVisible = false;
+
+    let nextSec = false; 
+
   </script>
 
 
@@ -70,7 +83,9 @@
 
         <div class="body-wrap-2"></div>
 
-        <div class="browser">
+        {#if browserVisible === true}
+
+        <div class="browser" transition:fly={{y: -50, duration: 600, easing: cubicInOut}}>
             <div class="browser-control">
                 <div class="button-1"></div>
                 <div class="button-2"></div>
@@ -79,15 +94,16 @@
             <div class="gpt">
                 <p>{outputText}</p>
             </div>
-
-            
         </div>
 
-        <p class="body-text">
+        {/if}
+
+        {#if nextSec === true}
+        <p class="body-text" transition:fly={{y: -20, duration: 800, easing: cubicInOut}}>
            Luckily, our university stakeholders had worked hard to address concerns and 
            questions from students and faculty about the use of AI at NAU. 
         </p>
-
+        {/if}
 
     </div>
 </body>
